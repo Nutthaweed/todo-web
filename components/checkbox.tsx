370a8d3b5cef5f01edee24266713cc08bcf404df
Nutthaweed <97228092+Nutthaweed@users.noreply.github.com>
2022-06-04 17:08:30 +0700

    commits

--- components/checkbox.tsx
+++ components/checkbox.tsx
@@ -1,16 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect} from 'react'
 import { useColorModeValue, Fade, useDisclosure, Button } from '@chakra-ui/react'
 import Todolist from './todolist'
 import AddTodo from './addtodo'
 
 const CheckboxAnimated: React.FC = () => {
   const  { isOpen, onToggle } = useDisclosure()
+  const todos = [
+    {
+        id: 1,
+        body: 'get rekt'
+    },
+    {
+        id: 2,
+        body: 'write todo web'
+    },
+    {
+        id: 3,
+        body: 'asdasdasd'
+    }
+]
+
+const [ Todo, setTodo] = useState(todos)
+
+function deleteTodo(id) {
+   const newTodos = Todo.filter(todo => {
+       return todo.id !== id
+   })
+   setTodo(newTodos)
+}
+
   return (
       <div className="justify-center items-center text-center flex gap-2 flex-col">
         <Button onClick={onToggle} colorScheme={useColorModeValue('cyan', 'purple')}>Open Todo</Button>
         <Fade in={isOpen} className='justify-center items-center text-center flex gap-2 flex-col py-10'>
         <Todolist />
-        <AddTodo />
+        <AddTodo todos={todos} deleteTodo={deleteTodo}/>
             </Fade>
       </div>
   )
--- components/todolist.tsx
+++ components/todolist.tsx
@@ -1,33 +1,19 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { CheckboxGroup, Checkbox, useColorModeValue, IconButton, HStack, Text, VStack, StackDivider, useColorMode} from '@chakra-ui/react'
 import { FaTrash } from 'react-icons/fa'
 
-const Todolist: React.FC = () => {
-    const todos = [
-        {
-            id: 1,
-            body: 'get rekt'
-        },
-        {
-            id: 2,
-            body: 'write todo web'
-        },
-        {
-            id: 3,
-            body: 'asdasdasd'
-        }
-    ]
+const Todolist: React.FC = (todos, deleteTodo) => { 
     return (
         <VStack divider={<StackDivider />} borderColor={useColorModeValue('cyan', 'purple')} borderWidth="2px" borderRadius="lg" 
         p="4" w="100%"
         >
-        {todos.map(todo => (
+        {todos.map((todo) => (
             <HStack key={todo.id}>
                 <CheckboxGroup colorScheme={useColorModeValue('cyan','purple')} size="lg" >
                 <Checkbox>
                     <Text>
                     {todo.body}
-                    <IconButton aria-label='Delete todo' icon={<FaTrash/>} isRound="true" />
+                    <IconButton aria-label='Delete todo' icon={<FaTrash/>} isRound="true" onClick={() => deleteTodo(todo.id)}  />
                     </Text>
                 </Checkbox>
            </CheckboxGroup>
